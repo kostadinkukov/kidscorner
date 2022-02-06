9701bd5a0a722160099eb200989657ab3e45c353
Kostadin Kukov <kostadinkukov22@abv.bg>
2022-02-06 14:26:11 +0200

    initial commit

--- api.js
+++ api.js
@@ -1,21 +1,25 @@
-async function request(url, options) {
-    try {
-        const response = await fetch(url, options);
+// async function request(url, options) {
+//     try {
+//         const response = await fetch(url, options);
 
-        if (response.ok == false) {
-            const error = await response.json();
-            throw new Error(error.mesage);
-        }
+//         if (response.ok == false) {
+//             const error = await response.json();
+//             throw new Error(error.mesage);
+//         }
 
-        if (response.status == 204) {
-            return response;
-        } else {
-            const data = await response.json();
-        }
+//         if (response.status == 204) {
+//             return response;
+//         } else {
+//             const data = await response.json();
+//         }
 
-    } catch (err) {
-        alert(err.message);
-        throw err;
-    }
+//     } catch (err) {
+//         alert(err.message);
+//         throw err;
+//     }
 
+// }
+
+function foo(){
+    console.log(Koci);
 }
